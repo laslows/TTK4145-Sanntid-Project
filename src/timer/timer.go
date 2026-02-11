05917a7b2d81f98ac192822f8d04ca1c1a256c68
MartheElisabeth <mehitlan@stud.ntnu.no>
2026-02-11 10:54:49 +0100

    feat: fixed some errors in timer module

--- src/timer/timer.go
+++ src/timer/timer.go
@@ -13,14 +13,14 @@ func New() *Timer {
 }
 
 func (t *Timer) Start(duration time.Duration) {
-    t.endTime = time.Now().Add(duration)
-    t.active = true
+    t.m_endTime = time.Now().Add(duration)
+    t.m_timerActive = true
 }
 
 func (t *Timer) Stop() {
-    t.active = false
+    t.m_timerActive = false
 }
 
 func (t *Timer) TimedOut() bool {
-    return t.active && time.Now().After(t.endTime)
+    return t.m_timerActive && time.Now().After(t.m_endTime)
 }
