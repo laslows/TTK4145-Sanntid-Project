a8e51cbce81514ed4a384b5e545a70e0d3aafe64
Anneli <annlsto@stud.ntnu.no>
2026-02-11 10:48:26 +0100

    chore: translates main.c to go

--- main.go
+++ main.go
@@ -2,20 +2,51 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	elevator "./src/elevator"
 	"./src/fsm"
 	initialize "./src/init"
 	"./src/network"
 	"./src/timer"
 )
 
+func pollLoop(inputPollRate_ms int, elev *elevator.Elevator) {
+	var prevBtn [elevator.N_FLOORS][elevator.N_BUTTONS]bool
+	prevFloor := -1
+
+	// Request Button
+	for f := 0; f < elevator.N_FLOORS; f++ {
+		for btn := 0; btn < elevator.N_BUTTONS; btn++ {
+			v := elevator.RequestButton(f, elevator.ButtonType(btn))
+			if v && v != prevBtn[f][btn] {
+				fsm.onRequestButtonPress(elev, f, btn)
+			}
+			prevBtn[f][btn] = v
+		}
+	}
+
+	// Floor Sensor TODO
+	f := elevator.FloorSensor()
+	if f != -1 && f != prevFloor {
+		fsm.onFloorArrival(elev, f)
+	}
+	prevFloor = f
+
+	// Door Timeout TODO
+	if Timer_timedOut() {
+		timer.stop()
+		fsm.onDoorTimeout(elev)
+	}
+
+	time.Sleep(time.Duration(inputPollRate_ms) * time.Millisecond)
+}
+
 func main() {
 	fmt.Printf("Started!\n")
 
-	//Elevator elevator = elevator_uninitialized()
-	//inputPollRate_ms int = 25
-
 	initialize.Initialize()
+	elev := elevator.New()
 	// -- Code Under --
 	/* LoadConfig("elevator.con",
 		ConVal("doorOpenDuration_s", "%d", &elevator.doorOpenDuration_s),
@@ -23,8 +54,8 @@ func main() {
 		ConVal("inputPollRate_ms", "%d", &elevator.inputPollRate_ms),
 	)
 
-	if(Elevator_floorSensor() == -1) {
-		Fsm_onInitBetweenFloors(&elevator);
+	if(elevator.FloorSensor() == -1) {
+		Fsm_onInitBetweenFloors(elev);
 	}
 
 	+ BUTTON_HANDLER?
@@ -33,4 +64,6 @@ func main() {
 	go fsm.Fsm()
 	go network.Network()
 	go timer.Timer()
+
+	go pollLoop(25, elev) // function translated from main.c in given code
 }
