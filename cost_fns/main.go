57a63530bf7e4fff461e483d3eacb7bf14d580a2
Anneli <annlsto@stud.ntnu.no>
2026-02-18 13:15:47 +0100

    feat: implements main.go for cost_fns

--- cost_fns/main.go
+++ cost_fns/main.go
@@ -1,27 +1,74 @@
 package costfns
 
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	config "./config"
+)
+
 type Input struct {
-	bool               [][]HallRequests
-	LocalElevatorState [string]States
+	HallRequests [][]bool                      `json:"hallRequests"`
+	States       map[string]LocalElevatorState `json:"states"`
 }
 
-func main(string []args) void {
+func main() {
 
-	string input_str;
+	var inputStr string
+	var clearReq string
 
+	flag.IntVar(&config.DoorOpenDuration, "doorOpenDuration", config.DoorOpenDuration, "Duration the door is open in milliseconds")
+	flag.IntVar(&config.TravelDuration, "travelDuration", config.TravelDuration, "Duration it takes to travel one floor in milliseconds")
+	flag.BoolVar(&config.IncludeCab, "includeCab", config.IncludeCab, "Whether to include cab requests in the cost function")
+	flag.StringVar(&clearReq, "clearReq", "inDirn", "Whether to clear all requests or only those in the direction of travel (all/inDirn)")
+	flag.StringVar(&inputStr, "input", "", "Input string in the format '[[hallReqs]], {elevatorStates}'")
+
+	flag.Parse()
+
+	switch clearReq {
+	case "All", "all":
+		config.SetClearRequestType(config.All)
+	case "InDirn", "inDirn":
+		config.SetClearRequestType(config.InDirn)
+	default:
+		fmt.Fprintf(os.Stderr, "Invalid clearReq value. Use 'all' or 'inDirn'.\n")
+		os.Exit(2)
+	}
 	//TODO: get DoorOpenDuration etc?
 
-	if input_str == string.init {
-		input_str = readln;
-		input_str = input_str[1:len(input_str)-1]; //removes trailing
+	if inputStr == "" {
+		b, err := io.ReadAll(bufio.NewReader(os.Stdin))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+			os.Exit(1)
+		}
+		inputStr = string(b)
 	}
 
-	Input i = inputstr.Input;
+	var in Input
+	if err := json.Unmarshal([]byte(inputStr), &in); err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing input: %v\n", err)
+		os.Exit(1)
+	}
 
 	//TODO: Fix main
+	//OptimalHallRequests := OptimalHallRequests(in.HallRequests, in.States)
+	hall := in.HallRequests
+	out := config.OptimalHallRequests(hall, in.States)
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetEscapeHTML(false)
+	if err := enc.Encode(out); err != nil {
+		fmt.Fprintf(os.Stderr, "Error encoding output: %v\n", err)
+		os.Exit(1)
+	}
 
 }
 
-unitest {
+/* unitest {s
 	//TODO: find corret go library and write some tests
-}
+} */
