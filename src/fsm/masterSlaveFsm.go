9d03d0757c55cec1c383d5f48a1b72b405456956
Espen <ebentdal@gmail.com>
2026-03-04 14:23:02 +0100

    feat: heartbeat-monitor might be working now

--- src/fsm/masterSlaveFsm.go
+++ src/fsm/masterSlaveFsm.go
@@ -9,31 +9,41 @@ import (
 )
 
 func MasterFsm(e *elevator.Elevator, hallButtonCh <-chan orders.Order, assignedOrderCh chan<- orders.Order,
-	changeMasterSlaveCh <-chan bool) {
+	updateWorldViewCh <-chan elevator.Backup, peerLostCh <-chan int) {
 Loop:
 	for {
 		select {
 		case buttonEvent := <-hallButtonCh:
 			//Should decide here who takes the order. For now it is just sent back to the fsm
 			chosenElevator := SuperOptimalOrderAssignmentAlgorithm()
-			
+
 			id := e.GetWorldView()[chosenElevator].GetID()
 
 			if id == e.GetID() {
 				assignedOrderCh <- buttonEvent
 			} else {
 				//Give to slave
-				network.SendHallOrder(buttonEvent, e.GetID(), 
-				id, network.HallOrderAssignment)
+				network.SendHallOrder(buttonEvent, e.GetID(),
+					id, network.HallOrderAssignment)
 			}
 
-		case isMaster := <-changeMasterSlaveCh:
-			if !isMaster {
+		case heartBeat := <-updateWorldViewCh:
+
+			e.UpdateWorldView(&heartBeat)
+			fmt.Printf("Updated worldview with heartbeat from %d received to %d\n", heartBeat.GetID(), e.GetID())
+
+			onUpdateWorldView(e)
+
+			if !e.GetIsMaster() {
 				fmt.Println("Switching to slave")
 				break Loop
-			} else {
-				fmt.Println("Should not be here, stay master")
 			}
+
+		case peer := <-peerLostCh:
+			fmt.Println("We lost peer ", peer)
+
+			e.LooseConnectionToPeer(peer)
+
 		}
 
 		time.Sleep(10 * time.Millisecond)
@@ -42,13 +52,14 @@ Loop:
 	//Maybe make onMasterSlaveChange-function
 	e.SetIsMaster(false)
 	e.UpdateMyBackup()
-	go SlaveFsm(e, hallButtonCh, assignedOrderCh, changeMasterSlaveCh)
+	go SlaveFsm(e, hallButtonCh, assignedOrderCh, updateWorldViewCh, peerLostCh)
 
 }
 
 // Kanskje vi kan returne fra masterFsm om vi bli slave, og starte denne. Og så motsatt ??
 // Idk om dette er en god løsning..
-func SlaveFsm(e *elevator.Elevator, hallButtonCh <-chan orders.Order, assignedOrderCh chan<- orders.Order, changeMasterSlaveCh <-chan bool) {
+func SlaveFsm(e *elevator.Elevator, hallButtonCh <-chan orders.Order, assignedOrderCh chan<- orders.Order, updateWorldViewCh <-chan elevator.Backup,
+	peerLostCh <-chan int) {
 
 	fmt.Println("I am slave")
 	fmt.Printf("Master is: %d \n", e.GetMasterID())
@@ -56,17 +67,32 @@ func SlaveFsm(e *elevator.Elevator, hallButtonCh <-chan orders.Order, assignedOr
 Loop:
 	for {
 		select {
-		case isMaster := <-changeMasterSlaveCh:
-			if isMaster {
-				fmt.Println("Switching to master")
-				break Loop
-			} else {
-				fmt.Println("Should not be here, stay slave")
-			}
 		case buttonEvent := <-hallButtonCh:
 			//Give to master
 			network.SendHallOrder(buttonEvent, e.GetID(), e.GetMasterID(), network.HallOrderRequest)
 
+		case heartBeat := <-updateWorldViewCh:
+
+			e.UpdateWorldView(&heartBeat)
+			fmt.Printf("Updated worldview with heartbeat from %d received to %d\n", heartBeat.GetID(), e.GetID())
+
+			onUpdateWorldView(e)
+
+			if e.GetIsMaster() {
+				fmt.Println("Switching to master")
+				break Loop
+			}
+
+		case peer := <-peerLostCh:
+			fmt.Println("We lost peer ", peer)
+
+			e.LooseConnectionToPeer(peer)
+
+			e.TryUpdateIsMaster()
+			if e.GetIsMaster() {
+				fmt.Println("Switching to master")
+				break Loop
+			}
 		}
 
 		time.Sleep(10 * time.Millisecond)
@@ -75,6 +101,15 @@ Loop:
 
 	e.SetIsMaster(true)
 	e.UpdateMyBackup()
-	go MasterFsm(e, hallButtonCh, assignedOrderCh, changeMasterSlaveCh)
+	go MasterFsm(e, hallButtonCh, assignedOrderCh, updateWorldViewCh, peerLostCh)
+
+}
+
+func onUpdateWorldView(e *elevator.Elevator) {
+
+	e.TryUpdateIsMaster()
+
+	//Also check motorstop
+	//Also check other stuff
 
 }
